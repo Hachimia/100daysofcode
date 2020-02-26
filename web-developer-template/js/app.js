004640faa5994d6380106ffe72f66834467eb634
Hachimia <hachimia.msa@gmail.com>
2020-02-26 10:34:51 +0100

    Web developer template
    Add a JS library for animations and media queries

--- web-developer-template/js/app.js
+++ web-developer-template/js/app.js
@@ -12,26 +12,3 @@ function typeWriter() {
 }
 
 window.onload = typeWriter;
-
-/*Fade in effect on sections when we scroll on the bottom of the page*/
-function Scroll (){
-  var presentation = document.getElementById('presentation');
-  var services = document.getElementById('services');
-  var portfolio = document.getElementById('portfolio');
-  var testimonials = document.getElementById('testimonials');
-  var contact = document.getElementById('contact');
-  var ypos = window.pageYOffset;
-
-if(ypos < 255){
-  presentation.style.opacity = "0";
-}
-else{
-  presentation.style.opacity = "1";
-  presentation.style.transition = "2s";
-}
-
-window.addEventListener('scroll', function(e){
-  console.log(ypos);
-});
-
-}
