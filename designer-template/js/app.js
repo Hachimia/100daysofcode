16cf7cf07cd4bdb532ca44e4e11bbdc13ed37d8d
Hachimia <hachimia.msa@gmail.com>
2020-03-25 19:12:55 +0100

    designer-template

--- designer-template/js/app.js
+++ designer-template/js/app.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.menu-button').click(function(){
         $('#top-menu').toggleClass('active');
     })
+
+    $('#submit').click(function(event){
+        event.preventDefault();
+      });
 });
