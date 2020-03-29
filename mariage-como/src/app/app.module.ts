0b0414f08416cdbb668b9b9838b4248f0200ffc7
Hachimia <hachimia.msa@gmail.com>
2020-03-29 19:57:02 +0200

    mobile app

--- mariage-como/src/app/app.module.ts
+++ mariage-como/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { UsersComponent } from './users/users.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    UsersComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
