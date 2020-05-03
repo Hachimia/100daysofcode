d21a731b08d0651665967b1ac2247e2bc980c75d
Hachimia <hachimia.msa@gmail.com>
2020-05-03 20:16:56 +0200

    firebase-project

---
+++ angular-firebase-crud/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import {OrdersService} from './shared/orders.service';
+import { ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OrdersComponent,
+    OrderListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [OrdersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
